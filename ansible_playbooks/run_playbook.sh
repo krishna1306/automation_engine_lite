0f104fec4e6bbd0138a29828aabe0e19b4499dfd
Krishna Bandi <kribandi@cisco.com>
2024-03-11 03:58:17 -0400

    added ansible log

--- ansible_playbooks/run_playbook.sh
+++ ansible_playbooks/run_playbook.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 value="$1"
+log_file="ansible.log"
+
 echo "Running bash script to change the level to --> $value"
 echo "Running Ansible Playbook now..."
-ansible-playbook ansible_playbooks/change_config.yml --extra-vars "var1=$value"
+
+ansible_output=$(ansible-playbook ansible_playbooks/change_config.yml --extra-vars "var1=$value" 2>&1)
+
+echo "$(date +"[%Y-%m-%d %H:%M:%S]") -- start" >> $log_file
+echo "$ansible_output" | while IFS= read -r line; do
+    echo "$line"
+done >> $log_file
+echo "$(date +"[%Y-%m-%d %H:%M:%S]") -- end" >> $log_file
